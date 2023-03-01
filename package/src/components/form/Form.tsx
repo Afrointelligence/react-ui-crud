39a574509530074e34826f94119507c701072d89
Chatis Santos <chatis@afrointelligence.com>
2023-03-01 06:01:36 -0600

    type improvement

--- package/src/components/form/Form.tsx
+++ package/src/components/form/Form.tsx
@@ -3,11 +3,15 @@ import { useForm, type UseFormReturn } from 'react-hook-form';
 import { FormProvider } from './FormProvider';
 
 interface Props<T> {
-  children: FC<UseFormReturn> | JSX.Element | JSX.Element[];
+  children: FC<UseFormReturn> | JSX.Element | (JSX.Element | JSX.Element[])[];
   onSubmit: (data: T) => Promise<void> | void;
   className?: string;
 }
-export function Form<T>({ children, onSubmit, className }: Props<T>): ReactElement {
+export function Form<T>({
+  children,
+  onSubmit,
+  className,
+}: Props<T>): ReactElement {
   const form = useForm();
   return (
     <div className={className}>
